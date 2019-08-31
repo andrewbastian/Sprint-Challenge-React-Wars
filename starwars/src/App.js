17b6e48616483e5b6b2e0c1c5a5adc5eee2b85f3
andrewbastian <andrew.bastian@gmail.com>
2019-08-31 15:27:04 -0700

    Server seems to be working now and data is in and working. MVP meet.Will submit my air table for this when I get a chance this evening.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,58 +1,39 @@
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
 import axios from "axios"
 
 import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
+
 import './App.css';
 import CharicterCards from "./components/CharicterCards.js"
+const Header = styled.h1`
 
-// function App() {
-//   const [data, setData] = useState([])
-//   const grab = () => {
-//     axios.get('https://swapi.co/api/people/?format=json')
-//       .then(response =>{
-//       setData(response.data.results)
-//       })
-//       .catch(error => {console.log("Error: Please try again later")}
-//     )
-// }
+}`
+    const CardConts = styled.div`
+text-align:center;
+        }`
+        const AppStyle = styled.h1`
+            display: flex;
+            justify-content: center;
+            align-items: space-evenly;
+            font-family: nasalization, sans-serif;
+            flex-flow: column;
+text-align: center;
 
-// useEffect(grab, [])
-// console.log(data)
+            }`
+
 function App() {
-  const[data, setData] = useState({});
-  useEffect(()=>{
-  axios.get('https://swapi.co/api/people')
-
-  .then(res => {
-     console.log(res.data.results);
-    setData(res.data.results)
-
-  })
-},[])
 
 return (
-  <div className="App">
-    <h1 className="Header">React Wars</h1>
-    <div>
-      // {data.map((item, index) => {
-      //
-      //   return <CharicterCards key = {index} name = {item.name}
-      //   birth_year = {item.birth_year}
-      //   hair_color = {item.hair_color}
-      //   height = {item.height}
-      //   eye_color = {item.eye_color}
-      //   gender = {item.gender} />
-      //
-      // })
-      // }
-      </div>
-  </div>
+    <AppStyle>
+    <Header>React Wars</Header>
+      <CardConts>
+      <CharicterCards />
+
+      })
+      }
+
+      </CardConts>
+    </AppStyle>
   )
 }
 
--- starwars/src/components/CharicterCards.js
+++ starwars/src/components/CharicterCards.js
@@ -1,24 +1,75 @@
-import React from "react";
+import axios from "axios"
+import styled from 'styled-components';
+
+import React, {useState, useEffect} from 'react';
+// import Films from "./Films.js"
+const Cards = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: space-evenly;
+    font-family: nasalization, sans-serif;
+    flex-flow: column;
+
+
+    }`
+    const Card = styled.div`
+font-family: nasalization, sans-serif;
+    display: flex;
+    justify-content: center;
+
+    border-top: 2px solid #FFE5C0;
+    border-bottom: 2px solid #FF3C5C;
+    margin-bottom: 5px;
+    width: 75%;
+        }`
+const Person = styled.h1`
+    display: flex;
+    flex-flow: column;
+
+    justify-content: center;
+    align-items: space-evenly;
+    text-shadow: 0 0 3px #0A0000, 0 0 5px #FF3C5C;
+    color: #FFE5C0;
+    }`
+    const Birth = styled.p`
+      color: #FFA72B;
+        }`
+        const Homeworld = styled.button`
+          color:  #17A694;
+          border: none;
+          background: none;
+          font-family: nasalization, sans-serif;
+          text-decoration: none;
+          font-size: 40px;
+            }`
 
 export default function CharicterCards(props) {
+  const[data, setData] = useState([{}]);
+  useEffect(()=>{
+  axios.get('https://swapi.co/api/people')
 
+  .then(res => {
+     console.log(res.data.results);
+    setData(res.data.results)
+
+  })
+},[])
 return (
+    <Cards>
+        {data.map(person => {
+            return <Card>
+                <Person>
+                  Name: <br/>  {person.name}
 
-    <div className= "Card">
+                  <Birth>  Birth Year: {person.birth_year} </Birth>
 
-    <CharicterCards>
-
-        <p>Name:{props.name}</p>
-        <p>{props.birth_year}</p>
-        <p>{props.hair_color}</p>
-        <p>{props.height}</p>
-        <p>{props.eye_color}</p>
-        <p>{props.gender}</p>
-
-
-    </CharicterCards>
-
-    </div>
-
-    )
+                    <Homeworld href = {person.homeworld}>
+                    <br/>
+                        Homeworld
+                    </Homeworld>
+                </Person>
+            </Card>
+        })}
+    </Cards>
+)
 }
