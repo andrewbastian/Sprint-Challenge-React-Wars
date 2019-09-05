46b13cfed9e5eaa4fad26a58f0f161f45cbc7db2
andrewbastian <andrew.bastian@gmail.com>
2019-09-04 18:19:16 -0700

    post 1v1 changes

--- starwars/src/components/Films.js
+++ starwars/src/components/Films.js
@@ -1,27 +1,16 @@
-// 
-// import axios from "axios"
-//
-// import React, {useState, useEffect} from 'react';
-// import CharicterCards from "./CharicterCards.js"
-// console.log(CharicterCards);
-// export default function Films(count,setter) {
-//   if (count < 0)
-//   setter([axios.get('https://swapi.co/api/people/1/')])
-// } else {
-//   axios.get(`https://swapi.co/api/people/${count}`)}
-//   .then(res => {
-//      console.log(res.data.results.films);
-//     setFilms(res.data.results.films)
-// }
-//
-//
-//
-//
-//
-//
-// return (
-//     <div>
-//
-//     </div>
-// )
-// }
+import axios from "axios"
+
+import React, {useState, useEffect} from 'react';
+import CharicterCards from "./CharicterCards.js"
+console.log(CharicterCards);
+export default function Films(count, setter) {
+  if (count < 0)
+    setter([axios.get('https://swapi.co/api/people/1/')])
+} else {
+  axios.get(`https://swapi.co/api/people/${count}`)
+}.then(res => {
+  console.log(res.data.results.films);
+  setFilms(res.data.results.films)
+}
+
+return (<div></div>)}
